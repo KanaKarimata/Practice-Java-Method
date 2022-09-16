2ddba73af6b014881a06a0690649541a880edd70
KanaKarimata <kana.lt22@gmail.com>
2022-09-16 15:26:26 +0900

    戻り値の利用

--- src/com/practice/Method.java
+++ src/com/practice/Method.java
@@ -3,17 +3,31 @@ package com.practice;
 public class Method {
     public static void main(String[] args) {
         System.out.println("メソッドを呼び出します");
+        int ans = add(100, 10);
+        System.out.println(ans);
+        //int returnValue = methodA();
+        //System.out.println(returnValue);
         //hello("Kana");
         //hello("Naoki");
-        add(100, 2);
-        add(100, 5);
+        //add(100, 2);
+        //add(100, 5);
         System.out.println("メソッドの呼び出しが終わりました");
     }
 
-    public static void add(int x, int y){
+    /*public static int methodA() {
+        int x = 100;
+        return x;
+    }*/
+
+    public static int add(int x, int y) {
+        int ans = x + y;
+        return ans;
+    }
+
+    /*public static void add(int x, int y){
         int ans = x + y;
         System.out.println(x + "+" + y + "=" + ans);
-    }
+    }*/
 
     /*public static void hello(String name) {
         System.out.println("Hello!" + name);
